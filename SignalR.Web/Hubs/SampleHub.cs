130c0a52c345a07d7ae3d80d598171599ce12755
Tunahan Ali Ozturk <72943708+Moongazing@users.noreply.github.com>
2024-03-06 21:02:16 +0300

    first message

---
+++ SignalR.Web/Hubs/SampleHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Web.Hubs;
+
+public class SampleHub : Hub
+{
+
+    public async Task BroadcastMessageToAllClient(string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessageForAllClient", message);
+    }
+}
---
+++ SignalR.Web/wwwroot/js/signalr-client.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+
+
+    const broadcastMessageToAllClientHubMethodCall = "BroadcastMessageToAllClient";
+    const receiveMessageForAllClientHubMethodCall = "ReceiveMessageForAllClient";
+    const connection = new signalR.HubConnectionBuilder().withUrl("/samplehub")
+        .configureLogging(signalR.LogLevel.Information).build();
+
+
+    function start() {
+        connection.start().then(() => console.log("Hub connection success."));
+    }
+
+
+    try {
+        start();
+    }
+    catch
+    {
+        setTimeout(() => start(), 5000);
+    }
+
+    connection.on(receiveMessageForAllClientHubMethodCall, (message) => {
+        console.log("Receive message", message);
+    })
+
+    $("#btn-send-message-all-client").click(function () {
+        const message = "hello world";
+
+        connection.invoke(broadcastMessageToAllClientHubMethodCall, message).catch(err => console.error("error", err));
+    })
+
+})
